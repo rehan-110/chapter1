aaef3a16707c69b1090aa795371c86deb93dd7ce
rehan-110 <ar4084480@gmail.com>
2024-09-06 16:01:36 +0500

    chapter one files

---
+++ app.js
@@ -0,0 +1,16 @@
+//chapter 1 alerts
+
+// Q1
+
+alert("Error! Please Enter The Valid Password");
+
+//Q2
+
+alert("Welcome To Js Land.. \n Happy Coding!");
+
+//Q3
+
+alert("Hello I can run JS through My Web Browser Console");
+
+//Q4
+//we Place Script Tag in  Head tag
